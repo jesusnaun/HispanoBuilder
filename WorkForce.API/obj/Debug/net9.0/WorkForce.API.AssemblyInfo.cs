f0b12f826b57910ebc86ccd68b71e9ed28aa1612
JESUS AGURTO <jesusnaun@outlook.com>
2025-08-05 23:57:25 -0500

    Ready?

--- WorkForce.API/obj/Debug/net9.0/WorkForce.API.AssemblyInfo.cs
+++ WorkForce.API/obj/Debug/net9.0/WorkForce.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkForce.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7947c357d8883b9e3e558090cf4958a3bc01edc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkForce.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkForce.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
