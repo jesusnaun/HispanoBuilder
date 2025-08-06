22663571abe73f0e9956914d6388939cd851e110
JESUS AGURTO <jesusnaun@outlook.com>
2025-08-05 23:39:30 -0500

    Initial commit

---
+++ WorkForce.WEB/Models/Employer/EmployerRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace WorkForce.WEB.Models.Employer
+{
+    public class EmployerRequest
+    {
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int StateId { get; set; }
+        public string County { get; set; }
+        public string Message { get; set; }
+    }
+}
